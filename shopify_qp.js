c606b1000627f88163230477b2270884e82bf1a3
Talha Shafiq <94887241+talhaqisstpay@users.noreply.github.com>
2022-02-20 18:51:42 +0500

    sjhsdf

--- shopify_qp.js
+++ shopify_qp.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
   
   
 // Overrides:
@@ -580,4 +580,4 @@ document.onreadystatechange = function(){
 
   var qisstpay_script_url="https://d13b6tz4fn4nn5.cloudfront.net/web/js/qpw.js?store_url="+qisstpay_current_url+"&qisstpay_shop_currency="+qisstpay_shop_currency+"&qisstpay_cart_currency="+qisstpay_cart_currency+"&qisstpay_product="+qisstpay_product_encoded+"&time="+qisstpay_current_timestamp;function qisstpay_loadjscssfile(t,s){var e;"js"==s?((e=document.createElement("script")).setAttribute("type","text/javascript"),e.setAttribute("src",t)):"css"==s&&((e=document.createElement("link")).setAttribute("rel","stylesheet"),e.setAttribute("type","text/css"),e.setAttribute("href",t)),void 0!==e&&document.getElementsByTagName("head")[0].appendChild(e)}qisstpay_loadjscssfile(qisstpay_script_url,"js");
   
-</script>
+
