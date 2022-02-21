b9c7d681f766fea2406d7af8dd9e77f528df3a5c
Talha Shafiq <94887241+talhaqisstpay@users.noreply.github.com>
2022-02-21 15:19:01 +0500

    slack webhook

--- shopify_qp.js
+++ shopify_qp.js
@@ -1,4 +1,22 @@
-  //Open one click checkout modal on product page
+function send_webhook_slack() {
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+var urlencoded = new URLSearchParams();
+urlencoded.append("payload", "{\"channel\": \"#shopify-errors\", \"username\": \"QP_Shopify\", \"text\": \"Error : Product ID Not Found\\nURL : \",\"icon_emoji\":\":ghost\"}");
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: urlencoded,
+  redirect: 'follow'
+};
+fetch("https://hooks.slack.com/services/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", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
+
+//Open one click checkout modal on product page
   function qisstpay_open_checkout() {
     
     console.log("Add Call")
@@ -11,7 +29,7 @@
                 const params = new URLSearchParams(window.location.search)
                 
                 var product_details = {
-                    'id':params.has('variant') ? params.get('variant') : document.getElementById('product-selectors') ? document.getElementById('product-selectors').value :  qisstpay_current_variant.id ,
+                   'id':params.has('variant') ? params.get('variant') : document.getElementById('product-selectors') ? document.getElementById('product-selectors').value :  qisstpay_current_variant.id ,
                     'form_type': 'product',
                     'utf8': '✓'
                 };
@@ -31,8 +49,13 @@
                 body: formBody
                 }).then(res => res.json())
                     .then(res => {
-                      console.log("Responsse",res);
+                     
+                      if(res.status == "bad_request"){
+                        console.log("Bad Request")
+                        send_webhook_slack();
+                      }
                       if(res){
+                        console.log("In Responseeee")
                         (document.getElementById('qp8911_bootstrapModal')).remove();
                         let qisstpay_merchant_token_cart = btoa(location.hostname);
                         var qisstpay_products = [];
