e5631f0bad4bc248a342883ae66af8d1c1458cea
Talha Shafiq <94887241+talhaqisstpay@users.noreply.github.com>
2022-02-21 17:41:13 +0500

    Update shopify_qp_prod.js

--- shopify_qp_prod.js
+++ shopify_qp_prod.js
@@ -1,3 +1,30 @@
+ async function getQisstPayMerchantToken(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+};
+
+
+async function getQpCartInfo(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json'
+    }});
+  return response.json(); // parses JSON response into native JavaScript objects}
+
+}; 
+
 function slack_hook(error){
   var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
